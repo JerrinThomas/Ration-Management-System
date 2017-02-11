d631178703949b77adf4782c238903a508b59446
azinmakaranth <azin.makaranth@gmail.com>
2017-02-11 13:39:52 +0530

    AdminPage2
    *Added Gitto's Panel !
    *Modify Admin Button-Visible Only To Super Admin

--- minipro/admin.php
+++ minipro/admin.php
@@ -6,17 +6,48 @@ header("location:login.php"); // Redirect to login.php page
 else //Continue to current page
 header( 'Content-Type: text/html; charset=utf-8' );
 require_once 'config.php';
-
+$username=$_SESSION['name'];
+$sql="SELECT role FROM admindetails WHERE username= '$username'";
+$result=mysqli_query($dbC,$sql);
+$ro=mysqli_fetch_row($result);
 ?>
 
 <!DOCTYPE html>
-<htmls>
 <head>
-    <title>Welcome To Admin Page Demonstration</title>
+    <meta charset="utf-8">
+    <title>admin</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="waves.css">
 </head>
-<body>
-    <h1>Welcome To Admin Page <?php echo $_SESSION['name']; ?></h1>
-    <p><a href="adminlogout.php">Logout</a></p> <!-- A link for the logout page -->
-    <p>Put Admin Contents</p>
-</body>
-</html>
+  <body>
+    <p></p>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li class="dropdown"><a href="#" class="dropdown-btn">Ration Shops</a>
+          <div class="dropdown-menu">
+              <a href="#">Add</a>
+              <a href="#">Remove</a>
+              <a href="#">Modify</a>
+          </div>
+      </li>
+      <li class="dropdown"><a href="#" class="dropdown-btn">Users</a>
+          <div class="dropdown-menu">
+              <a href="#">Add</a>
+              <a href="#">Remove</a>
+              <a href="#">Modify</a>
+          </div>
+      </li>
+      <li><a href="#">Stock</a></li>
+      <?php
+      // displays only if the admin is Super ! ..
+      if($ro[0]==1)
+            echo "<li class=\"dropdown\"><a href=\"^\" class=\"dropdown-btn\">Modify Admins</a><div class=\"dropdown-menu\"><a href=\"%\">Add</a><a href=\"%\">Remove</a><a href=\"%\">Modify</a></div></li>";
+      ?>
+      <a class="anbutton" href="adminlogout.php">Logout</a>
+      <script src="waves.js"></script>
+      <script type="text/javascript">
+          Waves.attach('.anbutton');
+          Waves.init();
+      </script>
+    </ul>
+  </body></html>
