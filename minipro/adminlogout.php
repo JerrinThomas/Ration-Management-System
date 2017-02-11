3ab1d4a8bb0f2fc4ef9c60c46f478732f0608e46
azinmakaranth <azin.makaranth@gmail.com>
2017-02-11 13:34:55 +0530

    Revert "AdminLogin"
    This reverts commit badb9b78a7189062226279b7a7d86c860603a593.

--- minipro/adminlogout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
- //Start the current session
-session_destroy(); //Destroy it! So we are logged out now
-header("location:adminlogin.php?msg=Successfully Logged Out... ");
-?>
