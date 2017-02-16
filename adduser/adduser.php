2e602a110b8f45a7d53119049e57b35603784ea3
azinmakaranth <azin.makaranth@gmail.com>
2017-02-16 18:10:42 +0530

    critical edits

---
+++ adduser/adduser.php
@@ -0,0 +1,72 @@
+
+<?php
+require_once('config.php');
+echo $_POST["name"];
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  $name=test_input($_POST["name"]);
+  $address1=test_input($_POST["address1"]);
+  $address2=test_input($_POST["address2"]);
+  $address3=test_input($_POST["address3"]);
+  $panchayat=test_input($_POST["panchayat"]);
+  $pin=test_input($_POST["pin"]);
+  $ward=test_input($_POST["ward"]);
+  $houseno=test_input($_POST["houseno"]);
+  $income=test_input($_POST["income"]);
+  $memberno=test_input($_POST["memberno"]);
+
+
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
+
+?>
