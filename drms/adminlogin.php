82b82da5250eb8b9533bd3498ba7a5747ac788a1
Gitto K Tomy <gittokpr@gmail.com>
2017-02-18 20:00:05 +0530

    new look for admin
    modified admin login page

--- drms/adminlogin.php
+++ drms/adminlogin.php
@@ -1,19 +1,32 @@
 <?php
 include_once 'check_login.php';
  ?>
-<!doctype html>
-<html>
+    <!doctype html>
+    <html>
+
     <head>
         <meta charset="UTF-8">
         <title>adminlogin</title>
+        <link rel="stylesheet" href="css/styleadmin.css">
+
     </head>
-    <body>
-      <form class="form-signin" name="form1" method="post">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input name="username" id="uname" type="text" class="form-control" placeholder="Username" autofocus>
-        <input name="password" id="pword" type="password" class="form-control" placeholder="Password">
-        <button name="Submit" id="submit" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <span><?php echo "<div>".$error."</div>"; ?></span>
-      </form>
+
+    <body class="align">
+        <div class="grid">
+            <form class="form-signin" name="form1" method="post">
+                <h2 class="form-signin-heading">Please sign in</h2>
+                <div class="form__field">
+                <input name="username" id="uname" type="text" class="form-control" placeholder="Username" autofocus>
+                </div>
+                <div class="form__field">
+                <input name="password" id="pword" type="password" class="form-control" placeholder="Password">
+                </div>
+                <div class="butt">
+                <button name="Submit" id="submit" class="button" type="submit">Sign in</button>
+                </div>
+                <span><?php echo "<h3>".$error."<h3>"; ?></span>
+            </form>
+        </div>
     </body>
-</html>
+
+    </html>
