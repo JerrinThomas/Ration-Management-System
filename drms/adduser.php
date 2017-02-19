49c55cf903a26cfcbde1978cc2e431ded7fc1f71
azinmakaranth <azin.makaranth@gmail.com>
2017-02-19 12:10:17 +0530

    edit

--- drms/adduser.php
+++ drms/adduser.php
@@ -1,6 +1,5 @@
 
 <?php
-
 require_once('config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
@@ -30,46 +29,86 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
            $adhar_no2[$i]=test_input($adhar_no2[$i]);
         }
  }
-
-
 }
+
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
+
 try {
+if(!isset($_FILES["fileToUpload"]["tmp_name"])||$hofamily==""||$adhar_no==""||$add1==""||$add2==""||$add3==""||$pan_mun_cor==""||$pincode==""||$wardno==""||$house_no==""||$monthly_in==""||$mob_no==""||$taluk=="") {
+     throw new Exception(" MISSING INPUT ..... <br> Try Again.... :/ ");
+  }
+foreach ($name as $k) {
+  if($k=="")
+    throw new Exception(" Missing Data In DETAILS OF MEMBERS :- Names Not Found... <br> Try Again :/ ..");
+}
+foreach ($adhar_no2 as $k) {
+  if($k=="")
+    throw new Exception(" Missing Data In DETAILS OF MEMBERS :- Adhar No Not Found... <br> Try Again :/ ..");
+}
+foreach ($age as $k) {
+  if($k=="")
+    throw new Exception(" Missing Data In DETAILS OF MEMBERS :- Age No Not Found... <br> Try Again :/ ..");
+}
+if((strlen((String)$adhar_no) !== 12))
+     throw new Exception(" Please Enter A Valid 12 Digit Adhar Number.... :/ <br>GO BACK>>> ");
+if((strlen((String)$mob_no) !== 10))
+          throw new Exception(" Please Enter A Valid 10 Digit Mobile Number.... :/ <br>GO BACK>>> ");
+if(count($name) < $no_of_mem)
+{
+     $c=$no_of_mem-count($name);
+     throw new Exception("Please Enter ".$c." More Member(s) :/ ....GO BACK>>> ");
+}
+if(count($name) > $no_of_mem)
+{
+     $c=$no_of_mem-count($name);
+     throw new Exception("Please Delete ".$c." Member(s).... :/ AND TRY AGAIN>>> ");
+}
+$ar=array();
+$e=1;
+ foreach ($adhar_no2 as $var) {
+  $len=strlen((String)$var);
+   if( $len !== 12)
+        {
+          $e=0;
+          array_push($ar,$var);
+        }
+ }
+
+
+if($e==0)
+{   echo "Please Enter A Valid Adhar No In The Member Details Section <br> The Following Adhar No.s Are Invalid... :/ ";
+    foreach ($ar as $v) {
+      echo "<br>$v<br>";
+    }
+    throw new Exception(" Please Go Back And Try Again ... :) ");
+
+}
+
+// Check if image file is a actual image or fake image
+if(empty($_FILES["fileToUpload"]["name"]))
+   {
+     throw new Exception(" No Image Found...");
+
+   }
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    if(!isset($_FILES["fileToUpload"]["tmp_name"])) {
-       throw new Exception("No Image Found..... \n Try Again.... ");
+$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+if($check == false) {
+    throw new Exception(" Sorry File of ".substr(strrchr($_FILES["fileToUpload"]["name"],'.'),1)." format,not an Image... :/ <br> Upload jpg or jpeg file...<br> Try Again <br> !.THANK YU.! ");
     }
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        throw new Exception("\n Please Go Back....");
-
-    } else {
-        throw new Exception(" Sorry File is not an Image...\n Upload jpg or jpeg file...\n Try Again \n !.THANK YU.! ");
-    }
-}
-// Check if file already exists
-/*
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-} */
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    throw new Exception("Sorry, your file is too large. Go Back...");
+    throw new Exception("Sorry, your file is too large. Go Back.. :/ .");
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "jpeg" ) {
-    throw new Exception("Sorry, only JPG, JPEG...Go Back... ");
+    throw new Exception("Sorry, only JPG, JPEG...Go Back... :/ ");
 }
 $chk="SELECT adhar_no,ration_card_no,mem_name FROM cardholder_and_mem WHERE adhar_no='$adhar_no'";
 $checkmem=mysqli_query($dbC,$chk);
@@ -88,7 +127,7 @@ if(mysqli_num_rows($checkmem) != 0){
 
   $result=mysqli_query($dbC,$sql);
   if(!$result) {
-     throw new Exception("Error In DataBase row addiction...\n Please Fill Valid Informations...\n Try Again By Going Back...");
+     throw new Exception("Error In DataBase row addiction...<br> Please Fill Valid Informations...<br> Try Again By Going Back... :/ ");
   }
   $sq="SELECT ration_card_no FROM rationcard_holder WHERE adhar_no='$adhar_no'";
   $res=mysqli_query($dbC,$sq);
@@ -98,10 +137,11 @@ if(mysqli_num_rows($checkmem) != 0){
        $ql="INSERT INTO cardholder_and_mem VALUES( ".$adhar_no2[$i].",".$rw[0].",'".$name[$i]."',".$age[$i].")";
         $qlout=mysqli_query($dbC,$ql);
         if(!$qlout) {
-          throw new Exception("Error In DataBase.MemberDetails row ".$i." addiction...\n Please Fill Valid Informations...\n Try Again By Going Back...");
+          throw new Exception("Error In DataBase.MemberDetails row ".$i." addiction...<br> Please Fill Valid Informations... ! <br> Try Again By Going Back... :/ ");
        }
 
  }
+
   echo "<img src=\"".$target_file."\"><br>The allotted ration card No : ".$rw[0]."<br>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>Head Of Family : ".$hofamily;
 } catch (Exception $e) {
   echo 'Caught exception: ',  $e->getMessage(), "\n  ";
