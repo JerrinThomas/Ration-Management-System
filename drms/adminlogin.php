dd5aea4927bdaf8a0d3941b0daa9ad747c8a849f
Gitto K Tomy <gittokpr@gmail.com>
2017-02-19 08:59:46 +0530

    small changes to admin login page error msg

--- drms/adminlogin.php
+++ drms/adminlogin.php
@@ -16,15 +16,17 @@ include_once 'check_login.php';
             <form class="form-signin" name="form1" method="post">
                 <h2 class="form-signin-heading">Please sign in</h2>
                 <div class="form__field">
-                <input name="username" id="uname" type="text" class="form-control" placeholder="Username" autofocus>
+                <input name="username" id="uname" type="text" class="form-control" placeholder="Username" required>
                 </div>
                 <div class="form__field">
-                <input name="password" id="pword" type="password" class="form-control" placeholder="Password">
+                <input name="password" id="pword" type="password" class="form-control" placeholder="Password" required>
                 </div>
                 <div class="butt">
                 <button name="Submit" id="submit" class="button" type="submit">Sign in</button>
                 </div>
-                <span><?php echo "<h3>".$error."<h3>"; ?></span>
+                <span><?php if($error)
+                        echo "<h3>".$error."</h3>"; ?>
+                </span>
             </form>
         </div>
     </body>
