26cafc958f121add59bbd089d2625dc942f5677f
azinmakaranth <azin.makaranth@gmail.com>
2017-03-25 22:09:59 +0530

    #(*@(%&&**())
    * extensive testing is must...

---
+++ mainpage/drmsadmin/rm_shops.php
@@ -0,0 +1,97 @@
+<?php
+include('includes/checksuper.php');
+include('includes/header.php');
+include('includes/nav.php');
+require_once('config.php');
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
+if(!isset($_SESSION['name']))
+  header("Location: adminlogin.php?msg=Sign In Again");
+if(isset($_POST["Submit"])) {
+    $shopno = $_POST["shopno"];
+    $sql="SELECT shopno,taluk from rationshops where shopno='$shopno'";
+    $re=mysqli_query($dbC,$sql);
+    $count=mysqli_num_rows($re);
+    $r=mysqli_fetch_row($re);
+    if($count==0){
+    $msg="Invalid Shop Number ! ";$msg1="";
+    }
+    elseif(strcasecmp($_SESSION['taluk'],$r[1]) != 0){
+      $msg="Ration Shop ".$shopno." Is Not Under Your Taluk ! ";$msg1="";
+    }
+    else {
+      $sql1="DELETE FROM rationshops WHERE shopno='$shopno'";
+      $res=mysqli_query($dbC,$sql1);
+      if($res==true)
+        $msg1="Shop ".$shopno." Removed.";
+      else {
+       $msg1="Unsuccessfull";
+      }
+      $msg="";
+    }
+}
+else {
+    $shopno="";
+    $msg="";
+    $msg1="";
+  }
+
+?>
+
+</body>
+<h2>Remove Ration Shop</h2>
+<div class="bfore">
+     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                    <span><?php echo "<h1>".$msg."</h1>" ?></span>
+                    <span class="input input--manami">
+        <input class="input__field input__field--manami" type="text" id="input-32" name="shopno" required/>
+        <label class="input__label input__label--manami" for="input-32">
+          <span class="input__label-content input__label-content--manami">Ration Shop Number</span>
+                    </label>
+                    </span>
+       <div>
+            <input class="btn" type="submit" value="submit" name="Submit" />
+        </div>
+        <span><?php echo "<h1>".$msg1."</h1>" ?></span>
+
+    </form>
+</div>
+<script src="js/custom-file-input.js"></script>
+        <script src="js/classie.js"></script>
+        <script>
+            (function () {
+                // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+                if (!String.prototype.trim) {
+                    (function () {
+                        // Make sure we trim BOM and NBSP
+                        var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+                        String.prototype.trim = function () {
+                            return this.replace(rtrim, '');
+                        };
+                    })();
+                }
+
+        [].slice.call(document.querySelectorAll('input.input__field')).forEach(function (inputEl) {
+                    // in case the input is already filled..
+                    if (inputEl.value.trim() !== '') {
+                        classie.add(inputEl.parentNode, 'input--filled');
+                    }
+
+                    // events:
+                    inputEl.addEventListener('focus', onInputFocus);
+                    inputEl.addEventListener('blur', onInputBlur);
+                });
+
+                function onInputFocus(ev) {
+                    classie.add(ev.target.parentNode, 'input--filled');
+                }
+
+                function onInputBlur(ev) {
+                    if (ev.target.value.trim() === '') {
+                        classie.remove(ev.target.parentNode, 'input--filled');
+                    }
+                }
+            })();
+        </script>
+</body>
