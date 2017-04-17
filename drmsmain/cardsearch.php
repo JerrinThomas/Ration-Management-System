b980f31d4fe4221ea841233856453f622cd05d8e
Gitto K Tomy <gittokpr@gmail.com>
2017-04-17 22:04:36 +0530

    rationcard search css

--- drmsmain/cardsearch.php
+++ drmsmain/cardsearch.php
@@ -23,23 +23,39 @@ if(isset($_GET["value"])){
     $chkres1=mysqli_query($dbC,$chk1);
     $row1=mysqli_fetch_row($chkres1);
     $out="<style>
+            @import url('https://fonts.googleapis.com/css?family=Asap|Questrial');
           body{
-               background: #3d4444;
-               color: #00ad15;
+               background: #181D27;
+               color: #F0EDEE;
+               font-family: 'Questrial', sans-serif;
           }
           table{
-           color:red;
+           color: #181d27;
+           background-color: white;
+           width: 100%;
+           height: 80px;
+           font-size: 20px;
           }
           tbody {
-              color:#CDDC39;
+              color:#F44336;
               overflow:scroll;
               height:100px;
           }
-          </style><body><center><h1>Search Result</h1></center><center><img src=\"drmsadmin/$row[12]\" /></center>
-          <div>
-            <center><h3>Head Of Family : $row[2] </h3></center>
+          #srchb2 {
+            width: 25%; height: 40px; background-color: #15a66e; border: none; margin: 10px; color: white; letter-spacing: 3px; font-size: 25px;
+          }
+          #srchb2:hover {
+          background-color:#DA344D;
+          }
+          </style><body>
+          
+          <center style=\" color: #181d27; background-color: white; height: 60px; margin-top: 2%; \"><h1 style=\" font-size: 50px; font-weight: 400; letter-spacing: 2px; \">Search Result</h1></center>
+          <div class=\"wht\" style=\" width: 50%; margin: 0 auto; margin-top: 5%; background-color: #FEFFFE; color: #181d27; height: 91%;\"><center>
+          <img src=\"drmsadmin/$row[12]\" style=\" margin: 25px; \"/></center>
+          <div class=\"inner\" style=\" background-color: #181d27; letter-spacing: 2px; width: 98%; margin: 0 auto; \">
+            <center style=\" color: white; \"><h3>Head Of Family : $row[2] </h3></center>
             <div>
-                     <center><h3>Monthly Balance</h3></center><center><table>
+                     <center style=\" color: white; \"><h3>Monthly Balance</h3></center><center><table>
                                         <thead>
                                             <tr>
                                                 <th>&nbsp&nbsp  Balance Rice  &nbsp&nbsp</th>
@@ -55,8 +71,8 @@ if(isset($_GET["value"])){
                                             </table>
             </div>
             <div> 
-            <center><h3>Your Ration Shop No is  $row[17] Under the Licensee $row1[1]</h3></center>
-                     <center><h3> Stock In Your Ration Shop ( $row[17] )</h3></center><center><table>
+            <center style=\" color: white; \"><h3>Your Ration Shop No is  $row[17] Under the Licensee $row1[1]</h3></center>
+                     <center style=\" color: white; \"><h3> Stock In Your Ration Shop ( $row[17] )</h3></center><center><table>
                                         <thead>
                                             <tr>
                                                 <th>&nbsp&nbsp  Balance Rice  &nbsp&nbsp</th>
@@ -72,6 +88,7 @@ if(isset($_GET["value"])){
                                             </table>
             </div>
             <center><a href=\"index.php\"><input type=\"submit\" name=\"srchb2\" id=\"srchb2\" value=\"Back\"></center>
+          </div>
           </div>
     
     ";   
